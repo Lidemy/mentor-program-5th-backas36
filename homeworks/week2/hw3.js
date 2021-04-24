8dbbd3c70bc76edc6266fc22a81b1a6fc19a15f9
Ashi <backas36@gmail.com>
2021-04-24 14:01:21 +0800

    week2 homework

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,11 @@
 function reverse(str) {
-  
+    let newStr="";
+    for (let i=(str.length-1);i>=0;i--){
+        newStr+=str[i];
+  }
+  console.log(newStr)
 }
 
-reverse('hello');
+reverse('1,2,3,2,1');
+reverse('yoyoyo');
+reverse('1abc2');
