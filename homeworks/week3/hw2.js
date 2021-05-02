9cd1caf382022d0921b0d3d6b01914fbb0f26de7
Ashi <backas36@gmail.com>
2021-05-02 12:09:36 +0800

    modify hw2.js

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,45 +1,47 @@
 const readline = require('readline')
 
-const lines = []
 const rl = readline.createInterface({
   input: process.stdin
 })
 
+const lines = []
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
 rl.on('line', (line) => {
   lines.push(line)
 })
 
-function solve(lines) {
-  const from = +lines[0].split(' ')[0]
-  const to = +lines[0].split(' ')[1]
-  // 先判斷是否為水仙花數
-  const check = (number) => {
-    const count = countD(number)
-    let sum = 0
-    let m = number
-    while (m !== 0) {
-      const num = m % 10
-      sum += num ** count
-      m = Math.trunc(m / 10)
-    }
-    return sum === number
-  }
-
-  // 判斷幾位數
-  const countD = (number) => {
-    let count = 0
-    while (number !== 0) {
-      number = Math.trunc(number / 10)
-      count++
-    }
-    return count
-  }
-
-  for (let i = from; i <= to; i++) {
-    if (check(i)) console.log(i)
+// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+const solve = (lines) => {
+  const n = +lines[0].split(' ')[0]
+  const m = +lines[0].split(' ')[1]
+  for (let i = n; i <= m; i++) {
+    if (flower(i)) console.log(i)
   }
 }
 
+const flower = (number) => {
+  const digit = countD(number)
+  let sum = 0
+  let number2 = number
+  while (number2 !== 0) {
+    const num = number2 % 10
+    sum += num ** digit
+    number2 = Math.trunc(number2 / 10)
+  }
+  return number === sum
+}
+
+const countD = (number) => {
+  let count = 0
+  while (number !== 0) {
+    number = Math.trunc(number / 10)
+    count++
+  }
+  return count
+}
+
+// 輸入結束，開始針對 lines 做處理
 rl.on('close', () => {
   solve(lines)
 })
