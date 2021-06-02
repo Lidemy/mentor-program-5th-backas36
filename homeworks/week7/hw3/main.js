25b36c9e026f34bdb57865c225b8b9ad24137d37
Ashi <backas36@gmail.com>
2021-06-02 22:08:17 +0800

    小修改 week7 hw3

--- homeworks/week7/hw3/main.js
+++ homeworks/week7/hw3/main.js
@@ -1,75 +1,31 @@
-const todoInput = document.querySelector('.todo-input')
-const addBtn = document.querySelector('.add-btn')
-const todoListUl = document.querySelector('.todo-list-ul')
+document.addEventListener('DOMContentLoaded', () => {
+  // common variables
+  const todoListUl = document.querySelector('.todo-list-ul')
+  const addBtn = document.querySelector('.add-btn')
+  const todos = getLocalTodoArr()
 
-// eventListener
-document.addEventListener('DOMContentLoaded', showTodos)
-addBtn.addEventListener('click', addTodo)
-todoListUl.addEventListener('click', removeTodo)
-
-// function
-function addTodo(event) {
-  event.preventDefault()
-  if (!todoInput.value) {
-    alert('不能空白唷 😏 ！！')
-    return
-  }
-  const todoLi = document.createElement('li')
-  const newTodo = document.createElement('span')
-  newTodo.textContent = todoInput.value
-  newTodo.classList.add('todo-item')
-  todoLi.appendChild(newTodo)
-
-  saveLocalTodos(todoInput.value)
-
-  const completedBtn = document.createElement('button')
-  completedBtn.innerHTML = '<i class="fas fa-check "></i>'
-  completedBtn.classList.add('completed-btn')
-  todoLi.appendChild(completedBtn)
-
-  const trashBtn = document.createElement('button')
-  trashBtn.innerHTML = '<i class="fas fa-trash "></i>'
-  trashBtn.classList.add('trash-btn')
-  todoLi.appendChild(trashBtn)
-
-  todoListUl.appendChild(todoLi)
-
-  todoInput.value = ''
-}
-
-function removeTodo(event) {
-  const item = event.target
-  if (item.classList[0] === 'trash-btn') {
-    removeLocalTodos(item.parentElement)
-    item.parentElement.remove()
-  }
-  if (item.classList[0] === 'completed-btn') {
-    item.previousElementSibling.classList.toggle('completed')
-  }
-}
-
-function getTodoArr() {
-  let todos
-  if (localStorage.getItem('todos') === null) {
-    todos = []
-  } else {
-    todos = JSON.parse(localStorage.getItem('todos'))
-  }
-  return todos
-}
-
-function saveLocalTodos(todo) {
-  const todos = getTodoArr()
-  todos.push(todo)
-  localStorage.setItem('todos', JSON.stringify(todos))
-}
-
-function showTodos() {
-  const todos = getTodoArr()
   todos.forEach((todo) => {
+    makeTodoElement(todo)
+  })
+
+  // eventListeners
+  addBtn.addEventListener('click', addTodo)
+  todoListUl.addEventListener('click', todoStatus)
+
+  // functions
+  function getLocalTodoArr() {
+    let todos
+    if (localStorage.getItem('todos') === null) {
+      todos = []
+    } else {
+      todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    return todos
+  }
+  function makeTodoElement(todoValue) {
     const todoLi = document.createElement('li')
     const newTodo = document.createElement('span')
-    newTodo.textContent = todo
+    newTodo.textContent = todoValue
     newTodo.classList.add('todo-item')
     todoLi.appendChild(newTodo)
 
@@ -84,12 +40,35 @@ function showTodos() {
     todoLi.appendChild(trashBtn)
 
     todoListUl.appendChild(todoLi)
-  })
-}
-
-function removeLocalTodos(todo) {
-  const todos = getTodoArr()
-  const todoStr = todo.childNodes[0].textContent
-  todos.splice(todos.indexOf(todoStr), 1)
-  localStorage.setItem('todos', JSON.stringify(todos))
-}
+  }
+  function addTodo(event) {
+    event.preventDefault()
+    const todoInput = document.querySelector('.todo-input')
+    if (!todoInput.value) {
+      alert('不能空白唷 😏 ！！')
+      return
+    }
+    makeTodoElement(todoInput.value)
+    saveLocalTodos(todoInput.value)
+    todoInput.value = ''
+  }
+  function saveLocalTodos(todo) {
+    todos.push(todo)
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+  function todoStatus(event) {
+    const item = event.target
+    if (item.classList[0] === 'trash-btn') {
+      removeLocalTodos(item.parentElement)
+      item.parentElement.remove()
+    }
+    if (item.classList[0] === 'completed-btn') {
+      item.previousElementSibling.classList.toggle('completed')
+    }
+  }
+  function removeLocalTodos(todo) {
+    const todoStr = todo.childNodes[0].textContent
+    todos.splice(todos.indexOf(todoStr), 1)
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+})
